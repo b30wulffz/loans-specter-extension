3b58ebd9cba4387517dd7707c55c78271110e3b3
b30wulffz <shlokpandey123@gmail.com>
2022-08-23 01:23:49 +0530

    update CurrentServiceStorageManager to use GenericDataManager

--- src/b30wulffz/specterext/loans/service.py
+++ src/b30wulffz/specterext/loans/service.py
@@ -1,6 +1,7 @@
 import logging
 
 from cryptoadvance.specter.services.service import Service, devstatus_alpha, devstatus_prod, devstatus_beta
+from cryptoadvance.specter.managers.genericdata_manager import GenericDataManager
 # A SpecterError can be raised and will be shown to the user as a red banner
 from cryptoadvance.specter.specter_error import SpecterError
 from flask import current_app as app
@@ -12,27 +13,19 @@ from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
-class CurrentServiceStorageManager:
-    def __init__(self) -> None:
-        self.data_file =  Path('{}/.specter_dev/common_service_storage.json'.format(Path.home()))
-        self.data_file.touch(exist_ok=True)
-        try:
-            with open(self.data_file) as json_file:
-                self.data = json.load(json_file)
-        except:
-            self.data = {}
-            with open(self.data_file, 'w') as json_file:
-                json.dump(self.data, json_file)
+class CurrentServiceStorageManager(GenericDataManager):
+    name_of_json_file = "common_service_storage.json"
 
     def update_common_service_data(self, service_id, value):
-        self.data[service_id] = value  
-        with open(self.data_file, 'w') as json_file:
-            json.dump(self.data, json_file)
+        self.data = {
+            service_id: value   
+        }  
+        self._save()
 
     def get_common_service_data(self, service_id):
         if service_id not in self.data:
             self.data[service_id] = {}
-        return self.data[service_id] 
+        return self.data[service_id]
 
 class LoansService(Service):
     id = "loans"
@@ -44,7 +37,7 @@ class LoansService(Service):
     blueprint_module = "b30wulffz.specterext.loans.controller"
     devstatus = devstatus_alpha
     isolated_client = False
-    currentServiceStorage = CurrentServiceStorageManager()
+    currentServiceStorage = CurrentServiceStorageManager(Path('{}/.specter_dev/'.format(Path.home())))
 
     # TODO: As more Services are integrated, we'll want more robust categorization and sorting logic
     sort_priority = 2
